37856f640439dce9ff7bc29b6c8c840d6ccdaa92
Scott Sperling <scsper@gmail.com>
2013-12-31 01:18:48 -0800

    adding search view logic

--- room_scheduler/campus/urls.py
+++ room_scheduler/campus/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, url
 from campus import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^attributes/$', views.attributes, name='attributes'),
+    url(r'^search/$', views.search, name='search')
 )
--- room_scheduler/campus/views.py
+++ room_scheduler/campus/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
-from campus.models import Room
+from campus.models import Room, Attribute
 
 def index(request):
     rooms = Room.objects.all()
     return render(request, 'campus/index.html', {'rooms': rooms})
+
+def attributes(request):
+    attributes = Attribute.objects.all()
+    return render(request, 'campus/attributes.html', {'attributes': attributes})
+
+def search(request):
+    attributes = request.GET.getlist('attributes')
+    rooms = Room.objects.all()
+
+    for attribute in attributes:
+        rooms = rooms.filter(attributes__name=attribute)
+
+    return render(request, 'campus/search.html', {'attributes': attributes})
