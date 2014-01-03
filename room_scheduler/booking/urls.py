de9f88b07be24b988e54f8c92a617148b0b5ab69
Ben Homer <bhomer77@gmail.com>
2014-01-02 19:23:11 -0800

    expanded booking views and tests

---
+++ room_scheduler/booking/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+
+from booking import views
+
+urlpatterns = patterns('',
+    url(r'^(?P<room_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<room_id>\d+)/create_event/$', views.create_event, name='create_event'),
+)
