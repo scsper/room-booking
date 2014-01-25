197c04dc204eff0cb225f09c535f2c5edc96ca28
Scott Sperling <scsper@gmail.com>
2014-01-25 00:49:03 -0800

    cleaning comments and prints

--- room_scheduler/booking/views.py
+++ room_scheduler/booking/views.py
@@ -19,26 +19,13 @@ def create_event(request, room_id):
 
 	if request.method == "POST":
 		form = CreateEventForm(request.POST)
-		print 'Raw Data: "%s"' % request.body
 		if form.is_valid():
 			form.save()
 			return HttpResponseRedirect(reverse('booking:detail', args=[room.id]))
 
-	# Create the Event
-	# event = Event.objects.create(name=name, notes=request.POST['notes'], setupTime=dtSetupTime, eventTime=dtEventTime, teardownTime=dtTeardownTime, endTime=dtEndTime)
-
-	# Attach attributes to Event
-	# for attribute in attributes:
-	#     event.attributes.add(Attribute.objects.get(name=attribute))
-
 	# # If event is reoccuring, add the series
 	# if series is not None:
 	#     event.series.add(series)
-
-	# # Attach the event to the room
-	# event.rooms.add(room)
-
-	##        return HttpResponseRedirect(reverse('booking:detail', args=(room.id,)))
 	else:
 		form = CreateEventForm()
 
@@ -47,6 +34,3 @@ def create_event(request, room_id):
 		'form': form,
 		'room': room
 	})
-
-    # return render(request, 'booking/create_event.html', {'room': room, 'attributes': room.attributes.all(), 'series': Series.objects.all()})
-
